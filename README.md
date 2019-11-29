0a503c535f91a3a6d1756b4a5c1b6dc8b800359d
Loïc Runarvot <loic.runarvot@trust-in-soft.com>
2019-11-29 02:00:07 +0100

    Prepare the repository for the first day.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # adventofcode-2019
+
+This repository contains the solution for the Advent of Code 2019. See https://adventofcode.com/2019
+
+All problem have been solved using *OCaml*, with the usage of *Lwt*.
---
+++ lib/setup_logs.ml
@@ -0,0 +1,41 @@
+(*****************************************************************************)
+(* The MIT License (MIT)                                                     *)
+(*                                                                           *)
+(* Copyright (c)                                                             *)
+(*   Loïc Runarvot                                                           *)
+(*                                                                           *)
+(* Permission is hereby granted, free of charge, to any person obtaining a   *)
+(* copy of this software and associated documentation files (the "Software"),*)
+(* to deal in the Software without restriction, including without limitation *)
+(* the rights to use, copy, modify, merge, publish, distribute, sublicense,  *)
+(* and/or sell copies of the Software, and to permit persons to whom the     *)
+(* Software is furnished to do so, subject to the following conditions:      *)
+(*                                                                           *)
+(* The above copyright notice and this permission notice shall be included   *)
+(* in all copies or substantial portions of the Software.                    *)
+(*                                                                           *)
+(* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS   *)
+(* OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF                *)
+(* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.    *)
+(* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY      *)
+(* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT *)
+(* OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR  *)
+(* THE USE OR OTHER DEALINGS IN THE SOFTWARE.                                *)
+(*****************************************************************************)
+
+let has_env_debug =
+  match Option.map String.lowercase_ascii (Sys.getenv_opt "AOC_DEBUG") with
+  | None -> false
+  | Some ("t" | "true" | "y" | "yes") -> true
+  | Some _ -> false
+
+let () =
+  Logs.set_reporter
+    (Logs.format_reporter
+       ~dst:Format.std_formatter
+       ~app:Format.std_formatter
+       ());
+  Logs.set_level
+    (if has_env_debug
+     then Some Logs.Debug
+     else Some Logs.App)
